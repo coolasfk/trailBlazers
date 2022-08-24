e8a1bda32f6a915e99e502ad57d68dbd8a8b4cbd
coolasfk <fk.przybyla@gmail.com>
2022-08-24 13:54:05 +0200

    commit

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const hamburger = document.getElementById('hamburger')
 const navMain = document.querySelector('.active')
 const current_rotation = 0;
 
+const submit_btn = document.querySelector('.submit_btn')
+
+
+
+
+const textarea = document.getElementById('growing_textfield');
+
+      textarea.addEventListener("keyup", e => {
+
+        textarea.style.height = "10px";
+
+        let scHeight = e.target.scrollHeight;
+        textarea.style.height = `${scHeight}px`;
+      });
+
+
+submit_btn.addEventListener('click', sendEmailFunction);
+
+function sendEmailFunction() {
+    var link = "mailto:whatevacreates@gmail.com"
+    + "?cc=fk.przybyla@gmail.com"
+    + "&subject=" + encodeURIComponent("hello .trailBlazers!")
+    + "&body=" + encodeURIComponent(textarea.value)
+;
+
+console.log(link)
+
+}
 
 
 hamburger.addEventListener('click', () => {
